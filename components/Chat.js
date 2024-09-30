514fa39204b55b76e252bc752a4fcfcb6a8e4278
Liz <51245710+eahowell@users.noreply.github.com>
2024-09-30 15:10:59 -0500

    Initialize Gifted Chat

--- components/Chat.js
+++ components/Chat.js
@@ -3,19 +3,42 @@ import {
   Text,
   View
 } from "react-native";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { GiftedChat } from "react-native-gifted-chat";
 
 const Chat = ({ route, navigation }) => {
   const { name, backgroundColor } = route.params;
+  const [messages, setMessages] = useState([]);
 
   useEffect(() => {
     navigation.setOptions({ title: name });
+    setMessages([
+      {
+        _id: 1,
+        text: "Hello developer",
+        createdAt: new Date(),
+        user: {
+          _id: 2,
+          name: "React Native",
+          avatar: "https://placeimg.com/140/140/any",
+        },
+      },
+    ]);
   }, []);
 
+  const onSend = (newMessages) => {
+    setMessages(previousMessages => GiftedChat.append(previousMessages, newMessages))
+  }
+
   return (
-    <View style={[styles.container, { backgroundColor }]}>
-      <Text>Hello {name}!</Text>
-    </View>
+    <GiftedChat
+      styles={[styles.container, { backgroundColor }]}
+      messages={messages}
+      onSend={messages => onSend(messages)}
+      user={{
+        _id: 1
+      }}
+    />
   );
 };
 
